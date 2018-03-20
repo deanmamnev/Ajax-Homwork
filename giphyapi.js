3ae218fbaeee1af357ece71782424672cfbba181
Dean Mamnev <deanmamnev@gmail.com>
2018-03-20 09:39:49 -0500

    trying again

--- giphyapi.js
+++ giphyapi.js
@@ -1,51 +1,52 @@
-var search = $(this).attr("image-input");
+var image = $(this).attr("#image-input");
 
 var images = ["Toy Story", "Bug's Life", "Toy Story 2", "Incredibles"];
 
 //BROKEN, GO BACK  var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ search +"?api_key=MefACdu3H6cYdsq06RCoHA7gmS8bJJUj";
 
 
-var queryURL = "http://api.giphy.com/v1/gifs/search?q="+ search +"&api_key=MefACdu3H6cYdsq06RCoHA7gmS8bJJUj&limit=10&"
+var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + image + "&api_key=MefACdu3H6cYdsq06RCoHA7gmS8bJJUj&limit=10&";
 
 console.log(queryURL);
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-      console.log(response);
-    });
+$.ajax({
+    url: queryURL,
+    method: "GET"
+}).then(function (response) {
+    console.log(response);
+});
 
 
-    //Button render function
+//Button render function
 
 
 
-     function renderButtons() {
+function renderButtons() {
 
-        // Deletes the image prior to adding new image
-        // (this is necessary otherwise you will have repeat buttons)
-        $("#buttons-view").empty();
-        // Loops through the array of image
-        for (var i = 0; i < images.length; i++) {
+    // Deletes the image prior to adding new image
+    // (this is necessary otherwise you will have repeat buttons)
+    $("#buttons-view").empty();
+    // Loops through the array of image
+    for (var i = 0; i < images.length; i++) {
 
-          // Then dynamicaly generates buttons for each image in the array
-          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-          var a = $("<button>");
-          // Adds a class of image to our button
-          a.addClass("image");
-          // Added a data-attribute
-          a.attr("data-name", images[i]);
-          // Provided the initial button text
-          a.text(images[i]);
-          // Added the button to the buttons-view div
-          $("#buttons-view").append(a);
-        }
-      }
+        // Then dynamicaly generates buttons for each image in the array
+        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+        var a = $("<button>");
+        // Adds a class of image to our button
+        a.addClass("image");
+        // Added a data-attribute
+        a.attr("data-name", images[i]);
+        // Provided the initial button text
+        a.text(images[i]);
+        // Added the button to the buttons-view div
+        $("#buttons-view").append(a);
+    }
+}
 
 //onclicks
-$("#add-image").on("click", function(event) {
+$("#add-image").on("click", function (event) {
     event.preventDefault();
+    console.log("CLICK WORKS");
     // This line of code will grab the input from the textbox
     var image = $("#image-input").val().trim();
 
@@ -54,17 +55,45 @@ $("#add-image").on("click", function(event) {
 
     // Calling renderButtons which handles the processing of our image array
     renderButtons();
-  });
+});
+
+
+$("#buttons-view").on("click", function (event) {
+    event.preventDefault();
+    console.log("Buttons CLICK WORKS");
+    // This line of code will grab the input from the textbox
+    var image = $("#image-input").val().trim();
 
 
 
-        //Add search topics
+    // Calling renderButtons which handles the processing of our image array
+
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + image + "&api_key=MefACdu3H6cYdsq06RCoHA7gmS8bJJUj&limit=10&";
+
+    console.log(queryURL);
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);    
+        // The image from the textbox is then added to our array
+    
+        var imagediv = $("<div id='imgDiv'>");
+        var imagebucket = $("<img>")
+        imagediv.attr("src", getGif.data[i].images.fixed_height_small.url)
+
+    });
+});
 
 
-        $(document).on("click", "",);
+//Add search topics
+
+
+$(document).on("click", "#image-input", );
 {
     $("images").empty();
-    console.log(results);
+    // console.log(results);
 
 
 
